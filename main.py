408948e086913aedeba959fa5c0f2d082b829970
miranda.yuan@outlook.com <mirandayuan@Mirandas-MacBook-Pro.local>
2023-11-27 12:18:57 +0100

    plot histogram

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 from math import log2
 from collections import Counter
+from matplotlib import pyplot as plt
 
 def parse_img(img):
     pixels = np.array(img.getdata())
@@ -10,11 +11,18 @@ def parse_img(img):
     dim = np.shape(pixels)[-1]
     return pixels.astype(int), dim
 
-
 def calc_pmf(arr, dimension):
     val, cnt = np.unique(arr, return_counts=True, axis=0)
     pmf = cnt / len(arr)
     myarr = np.hstack((val.reshape(-1,dimension),pmf.reshape(-1,1)))
+    values = myarr[:, :-1]  # Extract values from PMF (excluding the last column, which is the probability)
+    probabilities = myarr[:, -1]  # Extract probabilities from PMF
+
+    plt.bar(values.flatten(), probabilities, width=1.0)
+    plt.title(f'Histogram')
+    plt.xlabel('Pixel Value')
+    plt.ylabel('Probability')
+    plt.show()
     return myarr
 
 def calc_entropy(img):
@@ -26,49 +34,25 @@ def calc_entropy(img):
     return entropy
 
 def calc_joint_entropy(img1, img2):
-    set_a, dim_a = parse_img(img1)
-    set_b, dim_b = parse_img(img2)
+    set_a, _ = parse_img(img1)
+    set_b, _ = parse_img(img2)
     
-    # Combine the sets into pairs
     pairs = list(zip(set_a[:, 0], set_b[:, 0]))
-
-    # Count the occurrences of each pair
     pair_counts = Counter(pairs)
-
-    # Calculate joint probability
     total_pairs = len(pairs)
     joint_probability = {pair: count / total_pairs for pair, count in pair_counts.items()}
-
-    # Calculate joint entropy
     joint_entropy = -sum(probability * log2(probability) for probability in joint_probability.values())
-
-    # Print the joint entropy
     return joint_entropy
 
 def mutual_info(h_xy, h_x, h_y):
-    """
-    Calculate mutual information I(X;Y)
-
-    Parameters:
-    - h_xy: entropy of Y given X H(X,Y)
-    - h_x: entropy of X H(X)
-    - h_y: extropy of Y H(Y)
-
-    Returns:
-    - Mutual information I(X;Y)
-    """
     return h_x + h_y - h_xy
 
+def max_bit_rate(mi):
+    B = 500     # Hz
+    bit_rate = B*mi
+    return bit_rate
+
 def max_compression_ratio(img_type, entropy):
-    """
-    Calculate maximum compression ratio
-
-    Parameters:
-    - mi: Mutual information I(X;Y)
-
-    Returns:
-    - Max compression ratio
-    """
     size = 0
     if img_type == "colour":
         size = 3*8
@@ -76,21 +60,7 @@ def max_compression_ratio(img_type, entropy):
     if img_type == "gray-scale":
         size = 8
 
-    return entropy/size
-
-def max_bit_rate(mi):
-    """
-    Calculate maximum bit rate
-
-    Parameters:
-    - mi: Mutual information I(X;Y)
-
-    Returns:
-    - Max bit rate
-    """
-    B = 500     # Hz
-    bit_rate = B*mi
-    return bit_rate
+    return size/entropy
 
 if __name__ == "__main__":
     img_gs = Image.open('Images/ISAE_Logo_SEIS_gs.png', 'r')
